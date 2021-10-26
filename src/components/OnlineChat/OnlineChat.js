ebd37dbc1cbb3bf8270f5cd5f8fd4f4a299502f6
abduhakim <78894135+drizzy514@users.noreply.github.com>
2021-10-26 04:47:08 -0700

    Update OnlineChat.js

--- src/components/OnlineChat/OnlineChat.js
+++ src/components/OnlineChat/OnlineChat.js
@@ -4,7 +4,7 @@ import { useState } from "react"
 import Chat from "./Chat";
 import { Link } from "react-router-dom";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://localhost:3002");
 
 function OnlineChat() {
   const [username, setUsername] = useState("");
